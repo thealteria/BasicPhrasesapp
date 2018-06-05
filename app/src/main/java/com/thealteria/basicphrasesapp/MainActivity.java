b585647d22accf0aeeda521d41283b468cdb7cd1
TheAlteria <sarcasaman@gmail.com>
2018-06-05 19:51:39 +0530

    Initial commit

---
+++ app/src/main/java/com/thealteria/basicphrasesapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.thealteria.basicphrasesapp;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void btapped(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.thealteria.basicphrasesapp");
+
+
+            MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), resourceId);
+            mplayer.start();
+
+            Log.i("button tapped", ourId);
+
+
+
+    }
+
+
+}
